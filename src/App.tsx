40681bd3fccd311ed512a5195999fca8d5e0b23f
CALKO9611 <kimd9669@naver.com>
2024-01-12 16:53:59 +0900

    Outlet 사용하여 Layout 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import GuildMember from "./routes/guild-member";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "guild-member",
+        element: <GuildMember />,
+      },
+    ],
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;

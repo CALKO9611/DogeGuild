dfc718b81610223fe0db7b5d6ccdf6b7adee912b
CALKO9611 <kimd9669@naver.com>
2024-01-13 00:42:56 +0900

    users.json 연동하여 길드원 불러오기 구현

--- src/routes/guild-member.tsx
+++ src/routes/guild-member.tsx
@@ -1,43 +1,37 @@
-// GuildMember.tsx
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function GuildMember() {
-  const [memberInfo, setMemberInfo] = useState({
-    nickname: "",
-    level: "",
-    job: "",
-    characterImageUrl: "",
-  });
+  const [membersInfo, setMembersInfo] = useState([]);
 
   useEffect(() => {
-    const fetchMemberInfo = async () => {
+    const fetchMembersInfo = async () => {
       try {
-        const response = await axios.get("http://localhost:5174/guild-member");
-        const data = response.data;
-
-        setMemberInfo({
-          nickname: data.nickname,
-          level: data.level,
-          job: data.job,
-          characterImageUrl: data.characterImageUrl,
-        });
+        const response = await axios.get("http://localhost:5174/guild-member"); // 새로운 endpoint로 변경
+        setMembersInfo(response.data);
       } catch (error) {
         console.error("Failed to fetch member info:", error);
       }
     };
 
-    fetchMemberInfo();
+    fetchMembersInfo();
   }, []);
 
   return (
     <div>
       <h1>길드원 페이지입니다.</h1>
-      <p>용사의 이름: {memberInfo.nickname}</p>
-      <p>용사의 레벨: {memberInfo.level}</p>
-      <p>용사의 직업: {memberInfo.job}</p>
-      <img src={memberInfo.characterImageUrl} alt="캐릭터 이미지" />
+      {membersInfo.map((memberInfo, index) => (
+        <div key={index}>
+          <h1>주민등록상의 이름: {memberInfo.name}</h1>
+          <p>용사의 이름: {memberInfo.nickname}</p>
+          <p>용사의 레벨: {memberInfo.level}</p>
+          <p>용사의 직업: {memberInfo.job}</p>
+          <img
+            src={memberInfo.characterImageUrl}
+            alt={`${memberInfo.name}의 캐릭터 이미지`}
+          />
+        </div>
+      ))}
     </div>
   );
 }

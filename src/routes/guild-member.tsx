efa7f92b2a0a11140bb7aa26f7316fee116a1451
CALKO9611 <kimd9669@naver.com>
2024-01-15 18:57:52 +0900

    Loading 상태 추가

--- src/routes/guild-member.tsx
+++ src/routes/guild-member.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 interface MemberInfo {
   name: string;
@@ -9,7 +10,26 @@ interface MemberInfo {
   characterImageUrl: string;
 }
 
+const Wrapper = styled.div`
+  /* background-color: yellow; */
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* grid-template-rows: repeat(5, minmax(100px, auto)); */
+  margin: 0px 3.125rem;
+  gap: 0.625rem;
+  /* grid-gap: 20px; */
+`;
+
+const CharacterBox = styled.div`
+  /* background-color: yellow; */
+  display: flex;
+  border: 1px solid;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default function GuildMember() {
+  const [isLoading, setLoading] = useState(true);
   const [membersInfo, setMembersInfo] = useState<MemberInfo[]>([]);
 
   useEffect(() => {
@@ -17,10 +37,12 @@ export default function GuildMember() {
       try {
         const response = await axios.get<MemberInfo[]>(
           "http://localhost:5174/guild-member"
-        ); // 새로운 endpoint로 변경
+        ); // server.js 포트번호
+        setLoading(false);
         setMembersInfo(response.data);
       } catch (error) {
         console.error("Failed to fetch member info:", error);
+        setLoading(false);
       }
     };
 
@@ -28,20 +50,27 @@ export default function GuildMember() {
   }, []);
 
   return (
-    <div>
-      <h1>길드원 페이지입니다.</h1>
-      {membersInfo.map((memberInfo, index) => (
-        <div key={index}>
-          <img
-            src={memberInfo.characterImageUrl}
-            alt={`${memberInfo.name}의 캐릭터 이미지`}
-          />
-          <h1>이름: {memberInfo.name}</h1>
-          <p>닉네임: {memberInfo.nickname}</p>
-          <p>레벨: {memberInfo.level}</p>
-          <p>직업: {memberInfo.job}</p>
-        </div>
-      ))}
-    </div>
+    <Wrapper>
+      {/* <h1>길드원 페이지입니다.</h1> */}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        membersInfo.map((memberInfo, index) => (
+          <CharacterBox key={index}>
+            <img
+              src={memberInfo.characterImageUrl}
+              alt={`${memberInfo.name}의 캐릭터 이미지`}
+            />
+            <div>
+              <p>{memberInfo.name}</p>
+              {index === 0 && <p>길드마스터</p>}
+              <p>{memberInfo.nickname}</p>
+              <p>{memberInfo.level}</p>
+              <p>{memberInfo.job}</p>
+            </div>
+          </CharacterBox>
+        ))
+      )}
+    </Wrapper>
   );
 }

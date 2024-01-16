d1cd6a45e4f6990274e425992f5ff75bfecddcac
CALKO9611 <kimd9669@naver.com>
2024-01-16 23:15:07 +0900

    home.tsx CSS 초기화

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,18 +1,3 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  background-color: red;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
 export default function Home() {
-  return (
-    <Wrapper>
-      {/* <Banner /> */}
-      <h2>home</h2>
-    </Wrapper>
-  );
+  return <h2>home</h2>;
 }

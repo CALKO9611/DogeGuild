8cb596277b1cc8ed66f68e326a501802acaefb05
CALKO9611 <kimd9669@naver.com>
2024-03-26 18:43:06 +0900

    NEXON OPEN API 적용

--- src/routes/guild-member.tsx
+++ src/routes/guild-member.tsx
@@ -1,47 +1,75 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import member from "../../config/members.json";
 import styled from "styled-components";
 import CharacterBox from "../components/characterBox";
+
+interface CharacterData {
+  character_name: string;
+  character_level: number;
+  character_class: string;
+  character_image: string;
+  // world_name: string;
+  // character_gender: string;
+  // character_class_level: number;
+}
 
 const Wrapper = styled.div`
   /* background-color: yellow; */
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  /* grid-template-rows: repeat(5, minmax(100px, auto)); */
-  margin: 0px 3.125rem;
+  grid-template-rows: repeat(5, minmax(100px, auto));
+  margin: 3.125rem;
   gap: 0.625rem;
-  /* grid-gap: 20px; */
+  grid-gap: 20px;
 `;
 
 export default function GuildMember() {
-  const [isLoading, setLoading] = useState(true);
-  const [membersInfo, setMembersInfo] = useState([]);
+  const [responseData, setResponseData] = useState<CharacterData[] | null>(
+    null
+  );
+  const API_KEY = import.meta.env.VITE_MAPLESTORY_API_KEY;
 
   useEffect(() => {
-    const fetchMembersInfo = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:5174/guild-member"); // server.js 포트번호
-        setLoading(false);
-        setMembersInfo(response.data);
+        const headers = new Headers({
+          "x-nxopen-api-key": API_KEY || "",
+        });
+        const today = new Date();
+        today.setDate(today.getDate() - 1); // 하루 전 날짜로 설정
+        const formattedDate = `${today.getFullYear()}-${(
+          "0" +
+          (today.getMonth() + 1)
+        ).slice(-2)}-${("0" + today.getDate()).slice(-2)}`;
+        console.log(formattedDate);
+
+        const urls = Object.values(member).map((ocid) => {
+          return `https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${ocid}&date=${formattedDate}`;
+        });
+
+        // 모든 URL에 대해 각각 fetch를 수행합니다.
+        const responses = await Promise.all(
+          urls.map((url) => fetch(url, { headers }))
+        );
+
+        const datas: CharacterData[] = await Promise.all(
+          responses.map((response) => response.json())
+        );
+        setResponseData(datas);
       } catch (error) {
-        console.error("Failed to fetch member info:", error);
-        setLoading(false);
+        console.log(error);
       }
     };
-
-    fetchMembersInfo();
-  }, []);
+    fetchData();
+  }, [API_KEY]);
 
   return (
     <Wrapper>
-      {/* <h1>길드원 페이지입니다.</h1> */}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        membersInfo.map((memberInfo, index) => (
-          <CharacterBox key={index} index={index} memberInfo={memberInfo} />
-        ))
-      )}
+      {responseData &&
+        responseData.map((data: CharacterData, index: number) => (
+          <CharacterBox key={index} memberInfo={data} />
+        ))}
+      {!responseData && <p>Loading...</p>}
     </Wrapper>
   );
 }
